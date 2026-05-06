d8602497f9f944c54a635d9916d11cab8921328f
a1unade <tags0956@gmail.com>
2026-05-06 12:42:03 +0300

    [fix] auth header

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi;
 using Minio;
 using Planara.Common.Auth.Jwt;
 using Planara.Common.Configuration;
@@ -7,11 +8,39 @@ using Planara.Common.Database;
 using Planara.Common.Host;
 using Planara.Common.Validators;
 using Planara.Files.Data;
+using Planara.Files.Interfaces;
 using Planara.Files.Options;
+using Planara.Files.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddSettingsJson();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "Planara API",
+            Version = "v1"
+        });
+
+        options.AddSecurityDefinition("bearer", new OpenApiSecurityScheme
+        {
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT",
+            Description = "JWT Authorization header"
+        });
+
+        options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+        {
+            [new OpenApiSecuritySchemeReference("bearer", document)] = []
+        });
+    });
+}
+
 builder.Services
     .AddValidators(Assembly.GetExecutingAssembly())
     .AddHttpContextAccessor()
@@ -27,7 +56,8 @@ builder.Services.AddDataContext<DataContext>(
 );
 
 builder.Services.Configure<MinioOptions>(builder.Configuration.GetSection("Minio"));
-
+builder.Services.AddScoped<IFileService, FileService>();
+builder.Services.AddScoped<IObjectStorage, ObjectStorage>();
 builder.Services.AddSingleton<IMinioClient>(sp =>
 {
     var options = sp.GetRequiredService<IOptions<MinioOptions>>().Value;
@@ -43,6 +73,12 @@ builder.Services.AddSingleton<IMinioClient>(sp =>
 });
 
 var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseAuthentication();
 app.UseAuthorization();
